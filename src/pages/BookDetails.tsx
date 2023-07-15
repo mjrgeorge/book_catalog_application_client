61bf781e47fb67b7f3df6f98a103ba5025586462
mjrgeorge <mjrgeorge@gmail.com>
2023-07-16 01:17:02 +0600

    book delete done

--- src/pages/BookDetails.tsx
+++ src/pages/BookDetails.tsx
@@ -1,30 +1,105 @@
-import { useSingleBookQuery } from '@/redux/features/books/bookApi';
+import {
+  useDeleteBookMutation,
+  useSingleBookQuery,
+  useUpdateBookMutation,
+} from '@/redux/features/books/bookApi';
+import { IBook } from '@/types/globalTypes';
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
 import {
+  Backdrop,
   Button,
   Card,
   CardContent,
+  CircularProgress,
   Container,
+  Grid,
+  Paper,
   Stack,
   Typography,
 } from '@mui/material';
-import { useParams } from 'react-router-dom';
+import Dialog from '@mui/material/Dialog';
+import DialogContent from '@mui/material/DialogContent';
+import TextField from '@mui/material/TextField';
+import { ErrorMessage, Field, Form, Formik } from 'formik';
+import React from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import * as Yup from 'yup';
 
 const BookDetails = () => {
+  const navigate = useNavigate();
   const { id } = useParams();
   const { data } = useSingleBookQuery(id);
+
+  const [updateBook, { isLoading }] = useUpdateBookMutation();
+
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const initialValues: IBook = {
+    title: data?.data?.title,
+    author: data?.data?.author,
+    genre: data?.data?.genre,
+    publicationYear: data?.data?.publicationYear,
+    _id: 0,
+  };
+
+  const validationSchema = Yup.object().shape({
+    title: Yup.string().required('This is required'),
+    author: Yup.string().required('This is required'),
+    genre: Yup.string().required('This is required'),
+    publicationYear: Yup.number().required('This is required'),
+  });
+
+  const onSubmit = (values: IBook, props: { resetForm: () => void }) => {
+    const formReset = () => {
+      props.resetForm();
+    };
+    const options = {
+      id: id,
+      data: {
+        title: values.title,
+        author: values.author,
+        genre: values.genre,
+        publicationYear: values.publicationYear,
+      },
+    };
+
+    updateBook(options);
+    formReset();
+    handleClose();
+  };
+
+  const [deletePost, { isLoading: isDeleting }] = useDeleteBookMutation();
+
   return (
     <Container maxWidth="lg">
       <main>
         <Typography variant="h5" align="center" gutterBottom>
           Books Details
         </Typography>
-        <Stack direction="row" spacing={2} sx={{my: 3}}>
-          <Button variant="outlined" color="warning" startIcon={<EditIcon />}>
+        <Stack direction="row" spacing={2} sx={{ my: 3 }}>
+          <Button
+            variant="outlined"
+            color="warning"
+            startIcon={<EditIcon />}
+            onClick={handleClickOpen}
+          >
             Edit
           </Button>
-          <Button variant="outlined" color="error" startIcon={<DeleteIcon />}>
+          <Button
+            variant="outlined"
+            color="error"
+            startIcon={<DeleteIcon />}
+            onClick={() => deletePost(id).then(() => navigate('/'))}
+          >
             Delete
           </Button>
         </Stack>
@@ -54,7 +129,96 @@ const BookDetails = () => {
             </Typography>
           </CardContent>
         </Card>
+        <Dialog open={open} onClose={handleClose}>
+          <DialogContent>
+            <Container maxWidth="sm">
+              <Paper elevation={3} sx={{ p: 3 }}>
+                <Formik
+                  initialValues={initialValues}
+                  validationSchema={validationSchema}
+                  onSubmit={onSubmit}
+                  validateOnMount
+                >
+                  {(formik) => (
+                    <Form noValidate>
+                      <Grid container spacing={3}>
+                        <Grid item xs={12}>
+                          <Typography variant="h5" align="center" gutterBottom>
+                            New Book Entry
+                          </Typography>
+                        </Grid>
+                        <Grid item xs={12}>
+                          <Field
+                            as={TextField}
+                            name="title"
+                            label="Book Title"
+                            fullWidth
+                            error={formik.errors.title && formik.touched.title}
+                            helperText={<ErrorMessage name="title" />}
+                            required
+                          />
+                        </Grid>
+                        <Grid item xs={12}>
+                          <Field
+                            as={TextField}
+                            name="author"
+                            label="Book Author"
+                            fullWidth
+                            error={
+                              formik.errors.author && formik.touched.author
+                            }
+                            helperText={<ErrorMessage name="author" />}
+                            required
+                          />
+                        </Grid>
+                        <Grid item xs={12}>
+                          <Field
+                            as={TextField}
+                            name="genre"
+                            label="Book Genre"
+                            fullWidth
+                            error={formik.errors.genre && formik.touched.genre}
+                            helperText={<ErrorMessage name="genre" />}
+                            required
+                          />
+                          <Grid item xs={12}>
+                            <Field
+                              as={TextField}
+                              name="publicationYear"
+                              label="Publication Year"
+                              type="number"
+                              fullWidth
+                              error={
+                                formik.errors.publicationYear &&
+                                formik.touched.publicationYear
+                              }
+                              helperText={
+                                <ErrorMessage name="publicationYear" />
+                              }
+                              required
+                            />
+                          </Grid>
+                          <Grid item xs={12}>
+                            <Button type="submit" variant="contained" fullWidth>
+                              Save
+                            </Button>
+                          </Grid>
+                        </Grid>
+                      </Grid>
+                    </Form>
+                  )}
+                </Formik>
+              </Paper>
+            </Container>
+          </DialogContent>
+        </Dialog>
       </main>
+      <Backdrop
+        sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        open={isLoading || isDeleting}
+      >
+        <CircularProgress color="inherit" />
+      </Backdrop>
     </Container>
   );
 };
--- src/redux/features/books/bookApi.ts
+++ src/redux/features/books/bookApi.ts
@@ -11,14 +11,37 @@ const bookApi = api.injectEndpoints({
     }),
     postBook: builder.mutation({
       query: ({ data }) => ({
-        url: 'books/create-book',
+        url: '/books/create-book',
         method: 'POST',
         body: data,
       }),
       invalidatesTags: ['createBook'],
     }),
+    updateBook: builder.mutation({
+      query(data) {
+        const { id, ...body } = data;
+        return {
+          url: `/books/${id}`,
+          method: 'PATCH',
+          body,
+        };
+      },
+    }),
+    deleteBook: builder.mutation({
+      query(id) {
+        return {
+          url: `books/${id}`,
+          method: 'DELETE',
+        };
+      },
+    }),
   }),
 });
 
-export const { useGetBooksQuery, useSingleBookQuery, usePostBookMutation } =
-  bookApi;
+export const {
+  useGetBooksQuery,
+  useSingleBookQuery,
+  usePostBookMutation,
+  useUpdateBookMutation,
+  useDeleteBookMutation,
+} = bookApi;
